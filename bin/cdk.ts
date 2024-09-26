b1ee5915f206f618ed9d252e57dd6fd87f2218ce
tmokmss <tmokmss@users.noreply.github.com>
2024-09-26 09:59:37 +0900

    minor adjustments

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -6,11 +6,14 @@ import { DifyOnAwsStack } from '../lib/dify-on-aws-stack';
 const app = new cdk.App();
 new DifyOnAwsStack(app, 'DifyOnAwsStack', {
   env: {
-    region: 'ap-northeast-1',
+    region: 'us-west-2',
     // You need to explicitly set AWS account ID when you look up an existing VPC.
     // account: '123456789012'
   },
   // Allow access from the Internet. Narrow this down if you want further security.
   allowedCidrs: ['0.0.0.0/0'],
+  // Set Dify version
   difyImageTag: '0.8.3',
+
+  // Please see DifyOnAwsStackProps in lib/dify-on-aws-stack.ts for all the available properties
 });
