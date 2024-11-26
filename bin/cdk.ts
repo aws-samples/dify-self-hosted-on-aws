dfdf834c166a60e67ecab39c4a0ac6de129a9002
tmokmss <tmokmss@users.noreply.github.com>
2024-11-26 10:40:02 +0900

    update Dify to 0.12.0

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -13,7 +13,7 @@ new DifyOnAwsStack(app, 'DifyOnAwsStack', {
   // Allow access from the Internet. Narrow this down if you want further security.
   allowedCidrs: ['0.0.0.0/0'],
   // Set Dify version
-  difyImageTag: '0.9.1',
+  difyImageTag: '0.12.0',
 
   // uncomment the below for cheap configuration:
   // isRedisMultiAz: false,
