d47ab1614a7427fd2ffba4785be368d6a2011a97
yutaokos <yutaokos@amazon.co.jp>
2025-01-23 14:50:59 +0900

    fix: test case

--- test/dify-on-aws.test.ts
+++ test/dify-on-aws.test.ts
@@ -6,7 +6,7 @@ test('Snapshot test', () => {
   const app = new cdk.App();
   const stack = new DifyOnAwsStack(app, 'TestStack', {
     allowedIPv4Cidrs: ['0.0.0.0/0'],
-    allowedIPv4Cidrs: ['::/0'],
+    allowedIPv6Cidrs: ['::/0'],
     difySandboxImageTag: '0.2.4',
     domainName: 'example.com',
     hostedZoneId: 'Z0123456789ABCDEFG',
