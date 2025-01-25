ec134b904dc000da2814928b1e6971ee36c7fca0
tmokmss <tmokmss@users.noreply.github.com>
2025-01-25 12:13:14 +0900

    wip

--- bin/deploy.ts
+++ bin/deploy.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { BuildSpec, LinuxBuildImage, Project, Source } from 'aws-cdk-lib/aws-codebuild';
 import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'DeployStack', {
@@ -11,6 +12,10 @@ const stack = new cdk.Stack(app, 'DeployStack', {
   }),
 });
 const bucket = new Bucket(stack, 'SourceBucket');
+const logGroup = new LogGroup(stack, 'LogGroup', {
+  retention: RetentionDays.THREE_MONTHS,
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
+});
 const project = new Project(stack, 'Project', {
   environment: {
     buildImage: LinuxBuildImage.STANDARD_7_0,
@@ -20,6 +25,11 @@ const project = new Project(stack, 'Project', {
     path: 'code.zip',
   }),
   buildSpec: BuildSpec.fromSourceFilename('buildspec.yml'),
+  logging: {
+    cloudWatch: {
+      logGroup,
+    },
+  },
 });
 project.role!.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
 
@@ -28,4 +38,10 @@ new cdk.CfnOutput(stack, 'BucketName', {
 });
 new cdk.CfnOutput(stack, 'ProjectName', {
   value: project.projectName,
+});
+new cdk.CfnOutput(stack, 'ProgressUrlBase', {
+  value: `https://${cdk.Stack.of(stack).region}.console.aws.amazon.com/codesuite/codebuild/${cdk.Stack.of(stack).account}/projects/${project.projectName}/build`,
+});
+new cdk.CfnOutput(stack, 'LogGroupName', {
+  value: logGroup.logGroupName,
 });
--- simple-deploy.sh
+++ simple-deploy.sh
@@ -10,11 +10,16 @@ echo "Preparing CloudFormation stack $STACK_NAME..."
 npm ci
 npm run synth
 aws cloudformation deploy --stack-name $STACK_NAME --template-file deploy.yaml --capabilities CAPABILITY_IAM
-BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`BucketName`].OutputValue | [0]' --output text)
-PROJECT_NAME=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`ProjectName`].OutputValue | [0]' --output text)
+describeStack=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --output json)
+bucketName=$(echo $describeStack | jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "BucketName") | .OutputValue')
+projectName=$(echo $describeStack | jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "ProjectName") | .OutputValue')
+progressUrlBase=$(echo $describeStack | jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "ProgressUrlBase") | .OutputValue')
+logGroupName=$(echo $describeStack | jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "LogGroupName") | .OutputValue')
 
-echo "Found S3 bucket ${BUCKET_NAME}, CodeBuild project ${PROJECT_NAME}"
+echo "Found S3 bucket: ${bucketName}, CodeBuild project: ${projectName}"
 echo 
+echo "Preparation completed. Now proceed to deploy Dify on AWS."
+echo yes
 
 REPLY="no"
 
@@ -31,11 +36,11 @@ echo "Staging files..."
 # create archive of current files
 rm -f "/tmp/$ZIP_FILE_NAME"
 git ls-files | xargs zip -q "/tmp/$ZIP_FILE_NAME"
-aws s3 cp "/tmp/$ZIP_FILE_NAME" s3://${BUCKET_NAME}/code.zip
+aws s3 cp "/tmp/$ZIP_FILE_NAME" s3://${bucketName}/code.zip
 
 echo "Starting deployment..."
 
-buildId=$(aws codebuild start-build --project-name $PROJECT_NAME --query 'build.id' --output text)
+buildId=$(aws codebuild start-build --project-name $projectName --query 'build.id' --output text)
 
 if [[ -z "$buildId" ]]; then
     echo "Failed to start CodeBuild project"
@@ -43,6 +48,11 @@ if [[ -z "$buildId" ]]; then
 fi
 
 echo "Waiting for the CodeBuild project to complete..."
+echo "You can check the progress here:"
+echo "$progressUrlBase/$buildId"
+echo 
+
+aws logs tail --follow $logGroupName --format short--since 30s
 while true; do
     buildStatus=$(aws codebuild batch-get-builds --ids $buildId --query 'builds[0].buildStatus' --output text)
     if [[ "$buildStatus" == "SUCCEEDED" || "$buildStatus" == "FAILED" || "$buildStatus" == "STOPPED" ]]; then
@@ -51,3 +61,12 @@ while true; do
     sleep 10
 done
 echo "CodeBuild project completed with status: $buildStatus"
+echo 
+
+buildDetail=$(aws codebuild batch-get-builds --ids $buildId --output json)
+logGroupName=$(echo $buildDetail | jq -r '.builds[0].logs.groupName')
+logStreamName=$(echo $buildDetail | jq -r '.builds[0].logs.streamName')
+logs=$(aws logs get-log-events --log-group-name $logGroupName --log-stream-name $logStreamName)
+frontendUrl=$(echo "$logs" | grep -o 'DifyUrl = [^ ]*' | cut -d' ' -f3 | tr -d '\n' | tr -d '\n,')
+
+echo "Frontend URL: $frontendUrl"
