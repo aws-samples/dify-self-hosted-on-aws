2c087395c7e9f3b36376e9af1adc5246b5bdd445
MASHIKO Tatsuyoshi <m.tatsuyoshi@ragate.co.jp>
2025-03-20 00:29:53 +0900

    updated

--- lib/constructs/dify-services/api.ts
+++ lib/constructs/dify-services/api.ts
@@ -51,8 +51,8 @@ export class ApiService extends Construct {
     const volumeName = 'sandbox';
 
     const taskDefinition = new FargateTaskDefinition(this, 'Task', {
-      cpu: 2048,
-      memoryLimitMiB: 8192, // We got OOM frequently when RAM=512MB
+      cpu: 1024,
+      memoryLimitMiB: 2048, // We got OOM frequently when RAM=512MB
       runtimePlatform: { cpuArchitecture: CpuArchitecture.X86_64 },
       volumes: [
         {
