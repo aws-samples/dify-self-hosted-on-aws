bf50c8008eb5b896fc5e9543f2d0359b524be1b2
Taka Sasaki <kashira2339@gmail.com>
2025-11-27 13:49:07 +0900

    feat: add TRIGGER_URL and update paths for webhook callbacks in ApiService (#96)
    *Issue #, if available:*
    closes #95 
    
    *Description of changes:*
    Add support for Dify Trigger feature (v1.10.0+)
    
    I have verified the following before submitting this PR:
    
    - [x]  Snapshots updated via ``npm run test -- -u``
    - [x]  All tests passing (``npm run test``)
    - [x]  Code formatted (``npm run format``)
    - [x] Both changes tested and working as intended on my self-hosted
    instance
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -9,9 +9,9 @@ export const props: EnvironmentProps = {
   awsRegion: 'us-west-2',
   awsAccount: process.env.CDK_DEFAULT_ACCOUNT!,
   // Set Dify version
-  difyImageTag: '1.9.1',
+  difyImageTag: '1.10.1',
   // Set plugin-daemon version to stable release
-  difyPluginDaemonImageTag: '0.3.1-local',
+  difyPluginDaemonImageTag: '0.4.1-local',
 
   // uncomment the below options for less expensive configuration:
   // isRedisMultiAz: false,
